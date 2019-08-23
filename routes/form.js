c583417c407d517401ed244f9b69cf7c9a4ab4ba
Carlos Francisco Rodriguez Aguilar <crodriguez20@ucol.mx>
2019-08-23 17:38:57 -0500

    Fake request to provide fields

--- routes/form.js
+++ routes/form.js
@@ -4,9 +4,51 @@ const router = Router()
 
 router.route('/:formID/fields/')
   .get((req, res) => {
-    res.json({
-      foo: 'bar'
-    })
+    res.json([
+      {
+        _id: "5d6054f8351bcbefd274a0bd",
+        title: "Cipromox",
+        required: false,
+        name: "dolor",
+        config: {
+          type: 'number'
+        }
+      },
+      {
+        _id: "5d6054f876b841ee1176e945",
+        title: "Calcu",
+        required: false,
+        name: "dolor",
+        config: {
+          defaultValue: 'Default string'
+        }
+      },
+      {
+        _id: "5d6054f847e23edaff226eb6",
+        title: "Spacewax",
+        required: true,
+        name: "sint",
+        config: {
+          type: 'number',
+          minValue: 5,
+          maxValue: 10
+        }
+      },
+      {
+        _id: "5d6054f8f10b5dfdda7e2ace",
+        title: "Idealis",
+        required: true,
+        name: "aliqua",
+        config: {}
+      },
+      {
+        _id: "5d6054f84c8b0b7c82a82444",
+        title: "Skyplex",
+        required: false,
+        name: "do",
+        config: {}
+      }
+    ])
   })
 
 module.exports = router
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const { Router } = require('express')
 const form = require('./form')
+const cors = require('cors')
 
 const routes = Router()
 
+routes.use(cors({
+  origin: 'http://localhost:3000'
+}))
 routes.use('/form', form)
 
 routes.route('/')
