5ead84edb82be8b38288bfac129ec71b9457e364
Carlos Francisco Rodriguez Aguilar <crodriguez20@ucol.mx>
2019-08-25 01:13:48 -0500

    simulate distinct types

--- routes/form.js
+++ routes/form.js
@@ -20,6 +20,7 @@ router.route('/:formID/fields/')
         required: false,
         name: "dolor",
         config: {
+          type: 'text',
           defaultValue: 'Default string'
         }
       },
@@ -29,7 +30,7 @@ router.route('/:formID/fields/')
         required: true,
         name: "sint",
         config: {
-          type: 'number',
+          type: 'range',
           minValue: 5,
           maxValue: 10
         }
@@ -39,14 +40,90 @@ router.route('/:formID/fields/')
         title: "Idealis",
         required: true,
         name: "aliqua",
-        config: {}
+        config: {
+          type: 'text'
+        }
       },
       {
         _id: "5d6054f84c8b0b7c82a82444",
         title: "Skyplex",
         required: false,
         name: "do",
-        config: {}
+        config: {
+          type: 'tel'
+        }
+      },
+      {
+        _id: "5d6054f84c8b0b7c82a82333",
+        title: "Radio 1",
+        required: true,
+        name: "radio1",
+        config: {
+          type: 'radio',
+          items: [
+            'Perro',
+            'Gato',
+            'Ganzo'
+          ]
+        }
+      },
+      {
+        _id: "5d6054f84c8b0b7c82a82222",
+        title: "Radio 2",
+        required: false,
+        name: "radio2",
+        config: {
+          type: 'radio',
+          items: [
+            'Toalla',
+            'Almohada',
+            'Taza'
+          ],
+          defaultValue: 'Taza'
+        }
+      },
+      {
+        _id: "5d6054f84c8b0b7c82a82111",
+        title: "Checkbox 1",
+        required: true,
+        name: "checkbox1",
+        config: {
+          type: 'checkbox',
+          items: [
+            'Opcion 1',
+            'Opcion 2',
+            'Opcion 3'
+          ]
+        }
+      },
+      {
+        _id: "5d6054f84c8b0b7c82a81111",
+        title: "Checkbox 1",
+        required: true,
+        name: "checkbox2",
+        config: {
+          type: 'checkbox',
+          items: [
+            'me gusta la zanahoria',
+            'me gusta la papa',
+            'me gusta la brocoli'
+          ]
+        }
+      },
+      {
+        _id: "5d6054f84c8b0b7c82a80000",
+        title: "Select 1",
+        required: false,
+        name: "select1",
+        config: {
+          type: 'select',
+          items: [
+            'Patriots',
+            'Colts',
+            'Broncos',
+            'Saints'
+          ]
+        }
       }
     ])
   })
