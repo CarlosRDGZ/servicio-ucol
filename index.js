4c81df835496b82ee125c66b238e32613c295f6d
Carlos Francisco Rodriguez Aguilar <crodriguez20@ucol.mx>
2019-11-12 15:31:48 -0600

    asd

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require('express')
 const routes = require('./routes')
 const mongoose = require('mongoose')
-const { PORT, MONGODB_URI } = require('./config.json')
-
-const MONGO_URI = process.env.MONGODB_URI || MONGODB_URI
-const PORT_ = process.env.PORT || PORT
 
 const app = express()
 
-mongoose.connect(MONGO_URI, { useNewUrlParser: true }).catch(console.log)
+mongoose.connect('mongodb://148.213.20.112:27017/ciema', { useNewUrlParser: true }).catch(console.log)
 
 app.use(routes)
 
-app.listen(PORT_, () => console.log('Listening...'))
+app.listen(3000, () => console.log('Listening...'))
